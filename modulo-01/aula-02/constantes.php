b7a293441308626c6c4839f605c4b018d79740ed
Gustavo Galote <gusgalot3g@gmail.com>
2022-06-04 14:48:57 -0300

    feat: aulas 01 e 02

---
+++ modulo-01/aula-02/constantes.php
@@ -0,0 +1,9 @@
+<?php
+
+// Declarando com define
+define ('CPF' , '123.123.123-12');
+
+// Declarando com const
+const NOME_COMPLETO = "Gustavo";
+
+echo 'Nome: ' . NOME_COMPLETO . PHP_EOL . 'CPF: ' . CPF;
---
+++ modulo-01/aula-02/variaveis.php
@@ -0,0 +1,10 @@
+<?php
+
+$string = "Gustavo";
+$integer = 18;
+$float = 1.72;
+$boolean = true;
+
+if($boolean) {
+    echo "Me chamo ${string}, tenho ${integer} anos e ${float}cm de altura";
+}
