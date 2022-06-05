6ac1fce1051a4a94e598eaa9f6d187bd419e3a2e
Gustavo Galote <gusgalot3g@gmail.com>
2022-06-04 23:02:41 -0300

    feat: aula 10

---
+++ modulo-01/aula-10/strings.php
@@ -0,0 +1,13 @@
+<?php
+
+$nome = 'gustavo';
+
+$sobreNome = 'GALOTE';
+
+$nome2 = 'joao da silva';
+
+echo ucfirst($nome) . PHP_EOL;
+
+echo ucfirst(strtolower($sobreNome));
+
+echo ucwords($nome2);
